37ac7c4e2b2fbd360f8d7bf2e55e1223d1de70c1
jmindevelper <jmin.developer@gmail.com>
2022-08-01 18:45:39 +0900

    [init] 프로젝트 초기설정
    '
    - 스토리보드 제거
    - MineSweeperViewController 생성

---
+++ MineSweeper/MineSweeper/Scene/MineSweeperViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MineSweeperViewController.swift
+//  MineSweeper
+//
+//  Created by J_Min on 2022/08/01.
+//
+
+import UIKit
+
+final class MineSweeperViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+    
+}
