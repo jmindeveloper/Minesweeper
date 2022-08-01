6fb009b42a94c4db7c782115f87af9e72c8ddcda
jmindevelper <jmin.developer@gmail.com>
2022-08-01 19:03:39 +0900

    [feat] Minesweeper map을 위한 collectionView 생성 #2

---
+++ MineSweeper/MineSweeper/Scene/MineSweeperViewController/MineSweeperViewController.swift
@@ -0,0 +1,74 @@
+//
+//  MineSweeperViewController.swift
+//  MineSweeper
+//
+//  Created by J_Min on 2022/08/01.
+//
+
+import UIKit
+import SnapKit
+
+final class MineSweeperViewController: UIViewController {
+    
+    // MARK: - ViewProperties
+    private lazy var mineSweeperMapCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(
+            MineSweeperMapCollectionViewCell.self,
+            forCellWithReuseIdentifier: MineSweeperMapCollectionViewCell.identifier
+        )
+        
+        return collectionView
+    }()
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureSubViews()
+        setConstraintsOfMineSweeperMapCollectionView()
+    }
+}
+
+// MARK: - UI
+extension MineSweeperViewController {
+    private func configureSubViews() {
+        [mineSweeperMapCollectionView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+    }
+    
+    private func setConstraintsOfMineSweeperMapCollectionView() {
+        mineSweeperMapCollectionView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalToSuperview()
+            $0.height.equalToSuperview().multipliedBy(0.7)
+        }
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension MineSweeperViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 80
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: MineSweeperMapCollectionViewCell.identifier,
+            for: indexPath) as? MineSweeperMapCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension MineSweeperViewController: UICollectionViewDelegate {
+    
+}
